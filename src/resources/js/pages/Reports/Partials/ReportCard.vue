5db6b6472ced29cb58149453d75a715130281a9b
Madhukar Prabhakara <msprabhakara@Madhukars-MacBook-Pro.local>
2025-10-06 16:21:29 -0700

    url corrected for public reports

--- src/resources/js/pages/Reports/Partials/ReportCard.vue
+++ src/resources/js/pages/Reports/Partials/ReportCard.vue
@@ -11,13 +11,14 @@
       </div>
       <div class="flex shrink-0 self-center gap-2">
         <!-- Copy to Clipboard Badge Button -->
-         
-        <span
-          class="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
-          <ExternalLink class="mr-1 size-4" aria-hidden="true" />
 
-          {{ `/reports/${report.id}` }}
-        </span>
+        <a :href="`/reports/${report.uuid}`" target="_blank">
+          <span
+            class="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
+            <ExternalLink class="mr-1 size-4" aria-hidden="true" />
+            {{ `/reports/${report.uuid}` }}
+          </span>
+        </a>
         <Menu as="div" class="relative inline-block text-left">
           <MenuButton
             class="relative flex items-center rounded-full text-gray-400 outline-offset-[6px] hover:text-gray-600">
@@ -35,18 +36,18 @@
                 <MenuItem v-slot="{ active }">
                 <Link :href="`/reports/${report.id}/edit`"
                   :class="[active ? 'bg-gray-100 text-gray-900 outline-none' : 'text-gray-700', 'flex px-4 py-2 text-sm']">
-                  <Edit class="mr-3 size-5 text-gray-400" aria-hidden="true" />
-                  <span>Edit</span>
+                <Edit class="mr-3 size-5 text-gray-400" aria-hidden="true" />
+                <span>Edit</span>
                 </Link>
                 </MenuItem>
                 <MenuItem v-slot="{ active }">
                 <a href="#"
                   :class="[active ? 'bg-gray-100 text-gray-900 outline-none' : 'text-gray-700', 'flex px-4 py-2 text-sm']">
                   <Trash class="mr-3 size-5 text-gray-400" aria-hidden="true" />
-                  <span>Embed</span>
+                  <span>Delete</span>
                 </a>
                 </MenuItem>
-                
+
               </div>
             </MenuItems>
           </transition>
--- src/resources/js/pages/Reports/Partials/ReportsList.vue
+++ src/resources/js/pages/Reports/Partials/ReportsList.vue
@@ -15,7 +15,6 @@
 
 <script setup>
 import { ref } from 'vue'
-import { MoreVertical } from 'lucide-vue-next'
 import ReportCard from './ReportCard.vue'
 const openDropdown = ref(null)
 
@@ -23,24 +22,5 @@ function toggleDropdown(id) {
   openDropdown.value = openDropdown.value === id ? null : id
 }
 
-const clients = [
-  {
-    id: 1,
-    name: 'Tuple',
-    imageUrl: 'https://tailwindcss.com/plus-assets/img/logos/48x48/tuple.svg',
-    lastInvoice: { date: 'December 13, 2022', dateTime: '2022-12-13', amount: '$2,000.00', status: 'Overdue' },
-  },
-  {
-    id: 2,
-    name: 'SavvyCal',
-    imageUrl: 'https://tailwindcss.com/plus-assets/img/logos/48x48/savvycal.svg',
-    lastInvoice: { date: 'January 22, 2023', dateTime: '2023-01-22', amount: '$14,000.00', status: 'Paid' },
-  },
-  {
-    id: 3,
-    name: 'Reform',
-    imageUrl: 'https://tailwindcss.com/plus-assets/img/logos/48x48/reform.svg',
-    lastInvoice: { date: 'January 23, 2023', dateTime: '2023-01-23', amount: '$7,600.00', status: 'Paid' },
-  },
-]
+
 </script>
