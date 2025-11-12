3c958efd201bfa778a71c2ebf5a967b166345b11
Madhukar Prabhakara <msprabhakara@Madhukars-MacBook-Pro.local>
2025-11-12 09:36:07 -0800

    Production deploy

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\ReadCsvController;
-//Deploy live
+//Live deploy
 ini_set('max_execution_time', 1200);
 
 Route::get('/', function () {
